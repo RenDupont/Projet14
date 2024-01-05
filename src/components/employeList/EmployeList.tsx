38050f91ebb2c5d544ea3cebc732f8872ef916e4
Dupont <zeshin59@gmail.com>
2024-01-06 00:10:57 +0100

    add employeList and change Home form to fix issue

--- src/components/employeList/EmployeList.tsx
+++ src/components/employeList/EmployeList.tsx
@@ -1,31 +1,69 @@
 import Classes from './EmployeList.module.css';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-import $ from 'jquery';
+import DataTable, { TableColumn  } from "react-data-table-component";
+
+type DataRow = {
+    firstName: string;
+    lastName: string;
+    startDate: string;
+    department: number;
+    dateOfBirth: string;
+    street: string;
+    city: string;
+    state: string;
+    zipCode: number;
+};
+
 
 function EmployeList() {
 
-    useEffect(() => {
-        $('#employee-table').DataTable({
-            data: [],
-            columns: [
-                { title: 'First Name', data: 'firstName' },
-                { title: 'Last Name', data: 'lastName' },
-                { title: 'Start Date', data: 'startDate' },
-                { title: 'Department', data: 'department' },
-                { title: 'Date of Birth', data: 'dateOfBirth' },
-                { title: 'Street', data: 'street' },
-                { title: 'City', data: 'city' },
-                { title: 'State', data: 'state' },
-                { title: 'Zip Code', data: 'zipCode' },
-            ]
-        });
-    }, []);
+    const columns: TableColumn<DataRow>[] = [
+        {
+            name: 'First Name',
+            selector: row => row.firstName,
+        },
+        {
+            name: 'Last Name',
+            selector: row => row.lastName,
+        },
+        {
+            name: 'Start Date',
+            selector: row => row.startDate,
+        },
+        {
+            name: 'Department',
+            selector: row => row.department,
+        },
+        {
+            name: 'Date of Birth',
+            selector: row => row.dateOfBirth,
+        },
+        {
+            name: 'Street',
+            selector: row => row.street,
+        },
+        {
+            name: 'City',
+            selector: row => row.city,
+        },
+        {
+            name: 'State',
+            selector: row => row.state,
+        },
+        {
+            name: 'Zip Code',
+            selector: row => row.zipCode,
+        },
+    ];
 
     return (
         <div id="employee-div" className={Classes.container}>
             <h1>Current Employees</h1>
-            <table id="employee-table" className={Classes.display}></table>
+            <DataTable
+                columns={columns}
+                data={[]}
+                pagination
+            />
             <Link to={'/'}>Home</Link>
         </div>
     );
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,14 @@
 import Classes from './Home.module.css';
 import { Link } from "react-router-dom";
+import { states } from '../../assets/states';
 
 function Home() {
+
+    const handleSubmit = (event: React.SyntheticEvent) => {
+        alert('A name was submitted');
+        event.preventDefault();
+    }
+
     return (
         <>
             <div className={Classes.title}>
@@ -10,7 +17,7 @@ function Home() {
             <div className={Classes.container}>
                 <Link to={'/employe-list'}>View Current Employees</Link>
                 <h2>Create Employee</h2>
-                <form action="#" id="create-employee">
+                <form onSubmit={handleSubmit} id="create-employee">
                     <label htmlFor="first-name">First Name</label>
                     <input type="text" id="first-name" />
 
@@ -18,10 +25,10 @@ function Home() {
                     <input type="text" id="last-name" />
 
                     <label htmlFor="date-of-birth">Date of Birth</label>
-                    <input id="date-of-birth" type="text" />
+                    <input id="date-of-birth" type="date" />
 
                     <label htmlFor="start-date">Start Date</label>
-                    <input id="start-date" type="text" />
+                    <input id="start-date" type="date" />
 
                     <fieldset className={Classes.address}>
                         <legend>Address</legend>
@@ -33,7 +40,13 @@ function Home() {
                         <input id="city" type="text" />
 
                         <label htmlFor="state">State</label>
-                        <select name="state" id="state"></select>
+                        <select name="state" id="state">
+                            {states.map((state, index) => (
+                                <option key={index} value={state.abbreviation}>
+                                    {state.name}
+                                </option>
+                            ))}
+                        </select>
 
                         <label htmlFor="zip-code">Zip Code</label>
                         <input id="zip-code" type="number" />
@@ -47,9 +60,10 @@ function Home() {
                         <option>Human Resources</option>
                         <option>Legal</option>
                     </select>
+                    <div>
+                        <input type='submit' value="Save" />
+                    </div>
                 </form>
-
-                <button /*onClick={saveEmployee()}*/>Save</button>
             </div>
             <div id="confirmation" className={Classes.modal}>Employee Created!</div>
         </>
